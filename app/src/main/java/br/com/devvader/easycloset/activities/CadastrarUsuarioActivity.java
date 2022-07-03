5e466c2b47a2f20c499eb65c58f2f6e7f6e2cecf
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-07-03 17:04:25 -0400

    CadastrarUsuario - início da construção da activity de cadastro do usuário.

---
+++ app/src/main/java/br/com/devvader/easycloset/activities/CadastrarUsuarioActivity.java
@@ -0,0 +1,14 @@
+package br.com.devvader.easycloset.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import br.com.devvader.easycloset.R;
+
+public class CadastrarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_usuario);
+    }
+}
