e3ad6571a4bc196be9058e3da9a0c6cea4a257b1
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-07-04 19:59:58 -0400

    CadastrarUsuario - Novos ajustes nos campos do formulário e nas funcionalidades por trás. Inclusão de botão limpar.

--- app/src/main/java/br/com/devvader/easycloset/activities/CadastrarUsuarioActivity.java
+++ app/src/main/java/br/com/devvader/easycloset/activities/CadastrarUsuarioActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 import br.com.devvader.easycloset.R;
 import br.com.devvader.easycloset.domain.UsuarioEntity;
@@ -15,28 +16,67 @@ public final class CadastrarUsuarioActivity extends AppCompatActivity {
     private IUsuarioRepository usuarioRepository = new UsuarioRepository();
     private UsuarioEntity usuario;
 
+    EditText enderecoNomeUsuario;
+    EditText enderecoSobrenomeUsuario;
+    EditText enderecoCpfUsuario;
+    EditText enderecoFoneUsuario;
+    EditText enderecoEmailUsuario;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_usuario);
 
-        Button botaoSalvarUsuario = findViewById(R.id.button_salvarUsuario);
+        Button botaoSalvarUsuario = findViewById(R.id.button_salvarCadastroUsuario);
         botaoSalvarUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                capturarEnderecosDosCampos();
+                validarCamposObrigatorios();
                 criarUsuario();
                 salvarUsuario();
                 imprimirUsuario();
+                limparCamposCadastroUsuario();
             }
 
-                private void criarUsuario() {
-                    String nomeUsuario = findViewById(R.id.editText_nomeUsuario).toString();
-                    String sobrenomeUsuario = findViewById(R.id.editText_sobrenomeUsuario).toString();
-                    String cpfUsuario = findViewById(R.id.editText_cpfUsuario).toString();
-                    String foneUsuario = findViewById(R.id.editText_foneUsuario).toString();
-                    String emailUsuario = findViewById(R.id.editText_emailUsuario).toString();
+                private void capturarEnderecosDosCampos() {
+                    enderecoNomeUsuario = findViewById(R.id.editText_nomeUsuario);
+                    enderecoSobrenomeUsuario = findViewById(R.id.editText_sobrenomeUsuario);
+                    enderecoCpfUsuario = findViewById(R.id.editText_cpfUsuario);
+                    enderecoFoneUsuario = findViewById(R.id.editText_foneUsuario);
+                    enderecoEmailUsuario = findViewById(R.id.editText_emailUsuario);
+                }
 
-                    usuario = new UsuarioEntity(nomeUsuario, sobrenomeUsuario, cpfUsuario, foneUsuario, emailUsuario);
+                private void validarCamposObrigatorios() {
+                    if(enderecoNomeUsuario.getText().toString() == null ||
+                            enderecoNomeUsuario.getText().toString().trim().isEmpty()) {
+
+                        Toast.makeText(CadastrarUsuarioActivity.this,
+                                getString(R.string.nomeObrigatorioCadastroUsuario),
+                                Toast.LENGTH_LONG).show();
+
+                        enderecoNomeUsuario.requestFocus();
+                    }
+
+                    if(enderecoSobrenomeUsuario.getText().toString() == null ||
+                            enderecoSobrenomeUsuario.getText().toString().trim().isEmpty()) {
+
+                        Toast.makeText(CadastrarUsuarioActivity.this,
+                                getString(R.string.sobrenomeObrigatorioCadastroUsuario),
+                                Toast.LENGTH_LONG).show();
+
+                        enderecoSobrenomeUsuario.requestFocus();
+                    }
+                }
+
+                private void criarUsuario() {
+                    String nomeUsuario = enderecoNomeUsuario.getText().toString();
+                    String sobrenomeUsuario = enderecoSobrenomeUsuario.getText().toString();
+                    String cpfUsuario = enderecoCpfUsuario.getText().toString();
+                    String foneUsuario = enderecoFoneUsuario.getText().toString();
+                    String emailUsuario = enderecoEmailUsuario.getText().toString();
+                    usuario = new UsuarioEntity(
+                            nomeUsuario, sobrenomeUsuario, cpfUsuario, foneUsuario, emailUsuario);
                 }
 
                 private void salvarUsuario() {
@@ -45,11 +85,45 @@ public final class CadastrarUsuarioActivity extends AppCompatActivity {
 
                 private void imprimirUsuario() {
                     Toast.makeText(CadastrarUsuarioActivity.this,
-                            usuario.getNomeUsuario() + " " + usuario.getSobrenomeUsuario(),
-                            Toast.LENGTH_SHORT).show();
+                            usuario.getNomeUsuario().trim() + " " + usuario.getSobrenomeUsuario().trim(),
+                            Toast.LENGTH_LONG).show();
+                }
+
+                private void limparCamposCadastroUsuario() {
+                    enderecoNomeUsuario.setText(null);
+                    enderecoSobrenomeUsuario.setText(null);
+                    enderecoCpfUsuario.setText(null);
+                    enderecoFoneUsuario.setText(null);
+                    enderecoEmailUsuario.setText(null);
                 }
         });
 
+        Button botaoLimparCamposCadastroUsuario = findViewById(R.id.button_limparCadastroUsuario);
+        botaoLimparCamposCadastroUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                capturarEnderecosDosCampos();
+                limparCamposCadastroUsuario();
+
+                enderecoNomeUsuario.requestFocus();
+            }
+
+            private void capturarEnderecosDosCampos() {
+                enderecoNomeUsuario = findViewById(R.id.editText_nomeUsuario);
+                enderecoSobrenomeUsuario = findViewById(R.id.editText_sobrenomeUsuario);
+                enderecoCpfUsuario = findViewById(R.id.editText_cpfUsuario);
+                enderecoFoneUsuario = findViewById(R.id.editText_foneUsuario);
+                enderecoEmailUsuario = findViewById(R.id.editText_emailUsuario);
+            }
+
+            private void limparCamposCadastroUsuario() {
+                enderecoNomeUsuario.setText(null);
+                enderecoSobrenomeUsuario.setText(null);
+                enderecoCpfUsuario.setText(null);
+                enderecoFoneUsuario.setText(null);
+                enderecoEmailUsuario.setText(null);
+            }
+        });
 
     }
 }
