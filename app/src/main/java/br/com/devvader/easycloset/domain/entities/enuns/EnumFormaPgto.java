e07990678e983dd5f98850f7ca04924fb7d5fb96
juniorsmartins <juniorsoaresmartins@gmail.com>
2022-09-13 15:57:27 -0400

    Create - construção de Enum de formas de pagamento.

---
+++ app/src/main/java/br/com/devvader/easycloset/domain/entities/enuns/EnumFormaPgto.java
@@ -0,0 +1,13 @@
+package br.com.devvader.easycloset.domain.entities.enuns;
+
+public enum EnumFormaPgto {
+
+    DINHEIRO,
+    PIX,
+    TRANSFERENCIA_BANCARIA,
+    BOLETO_BANCARIO,
+    CARTAO_DEBITO,
+    CARTAO_CREDITO,
+    CREDIARIO,
+    CHEQUE;
+}
